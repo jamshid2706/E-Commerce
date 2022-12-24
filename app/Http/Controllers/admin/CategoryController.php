86c0576efd96e83b23782a38d1bb3d4209c4887d
Anthony Akbar <mamadjanovakbar@gmail.com>
2022-12-24 10:52:29 +0500

    Created Routes

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(){
+        dd('CategoryController Index ');
+        $categories = Category::all();
+        return view('admin.category.index', [$categories]);
+    }
+
+    public function show($id){
+        dd($id);
+        $post = Category::find($id);
+        return view('admin.category.show',[$post]);
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', [IndexController::class, 'index'])->name('admin.home');
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('admin.category');
+        Route::get('/{id}', [CategoryController::class, 'show'])->name('admin.category.show');
+    });
 });
