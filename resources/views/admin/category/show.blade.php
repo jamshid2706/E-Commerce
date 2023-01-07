4f17f2647407f156bc067b5d9808daa88742e5b7
ramziddin abdumominov <ramziddinabdumominov707@gmail.com>
2023-01-07 15:58:58 +0500

    Category fix

--- resources/views/admin/category/show.blade.php
+++ resources/views/admin/category/show.blade.php
@@ -142,7 +142,7 @@
                                 <div
                                     class="h-40 2xl:h-56 image-fit rounded-md overflow-hidden before:block before:absolute before:w-full before:h-full before:top-0 before:left-0 before:z-10 before:bg-gradient-to-t before:from-black before:to-black/10">
                                     <img alt="Midone - HTML Admin Template" class="rounded-md"
-                                         src="https://enigma.left4code.com/dist/images/preview-8.jpg">
+                                         src="{{asset('storage/'.$product->image)}}">
                                     <span
                                         class="absolute top-0 bg-pending/80 text-white text-xs m-5 px-2 py-1 rounded z-10">Featured</span>
                                     <div class="absolute bottom-0 text-white px-5 pb-6 z-10">
@@ -175,46 +175,19 @@
                                     Preview
                                 </a>
                                 <a class="flex items-center mr-3" {{--href="{{ route('admin.products.edit', $product->id) }}"--}}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                         stroke-linejoin="round" icon-name="check-square" data-lucide="check-square"
-                                         class="lucide lucide-check-square w-4 h-4 mr-1">
-                                        <polyline points="9 11 12 14 22 4"></polyline>
-                                        <path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"></path>
-                                    </svg>
+                                 <i data-lucide="check-square" class="mr-1"></i>
                                     Edit
                                 </a>
                                 <a class="flex items-center text-danger"
                                    {{--href="{{ route('admin.products.destroy', $product->id) }}"--}} data-tw-toggle="modal"
                                    data-tw-target="#delete-confirmation-modal">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                         stroke-linejoin="round" icon-name="trash-2" data-lucide="trash-2"
-                                         class="lucide lucide-trash-2 w-4 h-4 mr-1">
-                                        <polyline points="3 6 5 6 21 6"></polyline>
-                                        <path
-                                            d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"></path>
-                                        <line x1="10" y1="11" x2="10" y2="17"></line>
-                                        <line x1="14" y1="11" x2="14" y2="17"></line>
-                                    </svg>
+                                  <i data-lucide="trash-2" class="mr-1"></i>
                                     Delete
                                 </a>
                             </div>
                         </div>
                     </div>
                 @endforeach
-                <a href="javascript:;" data-tw-toggle="modal" data-tw-target="#add-item-modal"
-                   class="intro-y block col-span-12 sm:col-span-4 2xl:col-span-3">
-                    <div class="box rounded-md p-3 relative zoom-in">
-                        <div class="flex-none relative block before:block before:w-full before:pt-[100%]">
-                            <div class="absolute top-0 left-0 w-full h-full image-fit">
-                                <img alt="Midone - HTML Admin Template" class="rounded-md"
-                                     src="https://enigma.left4code.com/dist/images/food-beverage-4.jpg">
-                            </div>
-                        </div>
-                        <div class="block font-medium text-center truncate mt-3">Root Beer</div>
-                    </div>
-                </a>
             </div>
         </div>
     </div>
