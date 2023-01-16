45c477483e74bca969eba248650ea308862ae2af
Anthony Akbar <mamadjanovakbar@gmail.com>
2023-01-16 15:05:45 +0500

    sidebar

---
+++ resources/views/mobile/partials/sidebar.blade.php
@@ -0,0 +1,51 @@
+<div class="menu-size" style="width:230px;">
+    <div class="pt-3">
+        <div class="page-title sidebar-title d-flex">
+            <div class="align-self-center me-auto">
+                <h1>Royal</h1>
+            </div>
+            <div class="align-self-center ms-auto">
+                <a href="#"
+                   data-bs-toggle="dropdown"
+                   class="icon gradient-blue shadow-bg shadow-bg-s rounded-m">
+                    <svg>
+                        <image style="fill: white; padding: 15px" class="rounded-m" width="30" src="svgexport-13.svg"/>
+                    </svg>
+                </a>
+
+                <div class="dropdown-menu">
+                    <div class="card card-style shadow-m mt-1 me-1">
+                        <div class="list-group list-custom list-group-s list-group-flush rounded-xs px-3 py-1">
+                            <a href="page-wallet.html" class="list-group-item">
+                                <i class="has-bg gradient-green shadow-bg shadow-bg-xs color-white rounded-xs bi bi-credit-card"></i>
+                                <strong class="font-13">Wallet</strong>
+                            </a>
+                            <a href="page-activity.html" class="list-group-item">
+                                <i class="has-bg gradient-blue shadow-bg shadow-bg-xs color-white rounded-xs bi bi-graph-up"></i>
+                                <strong class="font-13">Activity</strong>
+                            </a>
+                            <a href="page-profile.html" class="list-group-item">
+                                <i class="has-bg gradient-yellow shadow-bg shadow-bg-xs color-white rounded-xs bi bi-person-circle"></i>
+                                <strong class="font-13">Account</strong>
+                            </a>
+                            <a href="page-signin-1.html" class="list-group-item">
+                                <i class="has-bg gradient-red shadow-bg shadow-bg-xs color-white rounded-xs bi bi-power"></i>
+                                <strong class="font-13">Log Out</strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="list-group list-custom list-menu-large">
+        <a class="list-group-item" data-toggle-theme data-trigger-switch="switch-1">
+            <i class="pt-5 bi bg-yellow-dark shadow-bg shadow-bg-xs bi-lightbulb-fill"></i>
+            <div>Dark Mode</div>
+            <div class="form-switch ios-switch switch-green switch-s me-2">
+                <input type="checkbox" data-toggle-theme class="ios-input" id="switch-1">
+                <label class="custom-control-label" for="switch-1"></label>
+            </div>
+        </a>
+        </div>
+    </div>
+</div>
