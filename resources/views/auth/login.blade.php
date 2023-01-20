1bb4d1d43d0d5fea98124142063f8dd31ba1e438
Anthony Akbar <mamadjanovakbar@gmail.com>
2023-01-20 08:44:25 +0500

    Login page change

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,17 @@
 @extends('layouts.app')
 @section('content')
-    <div class="login">
+    <div style="height: 100vh" class="login">
         <div class="container sm:px-10">
             <div class="block xl:grid grid-cols-2 gap-4">
-                <!-- BEGIN: Login Info -->
                 <div class="hidden xl:flex flex-col min-h-screen">
-                    <a href="" class="-intro-x flex items-center pt-5">
-                        <img alt="Midone - HTML Admin Template" class="w-6"
-                             src="{{ asset('dist/images/logo.svg') }}">
-                        <span class="text-white text-lg ml-3">
-                        Edusys
-                    </span>
-                    </a>
                     <div class="my-auto">
-                        <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16"
-                             src="/svgexport-13.svg">
-                        <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">A few more clicks to <br> sign
-                            in to your account.</div>
+                        <img alt="Royal" class="-intro-x w-1/2 -mt-16" src="/svgexport-1.svg">
                     </div>
                 </div>
-                <!-- END: Login Info -->
-                <!-- BEGIN: Login Form -->
                 <form action="{{ route('login') }}" method="POST">
                     @csrf
                     <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-4">
-                        <div
-                            class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
+                        <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
                             <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left ">Sign In</h2>
                             <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">A few more clicks to sign in to your
                                 account. Manage all your e-commerce accounts in one place</div>
