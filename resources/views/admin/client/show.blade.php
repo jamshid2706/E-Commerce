eb26783466210dd1569c09b71ac113c2ca218c21
ramziddin abdumominov <ramziddinabdumominov707@gmail.com>
2023-02-09 11:22:29 +0500

    show blade image togillandi

--- resources/views/admin/client/show.blade.php
+++ resources/views/admin/client/show.blade.php
@@ -7,11 +7,8 @@
     <div class="intro-y box px-5 pt-5 mt-5">
         <div class="flex flex-col lg:flex-row border-b border-slate-200/60 pb-5 -mx-5">
             <div class="flex flex-1 px-5 items-center justify-center lg:justify-start">
-                <div class="w-20 h-20 sm:w-24 sm:h-24 flex-none lg:w-32 lg:h-32 image-fit relative">
-                    <img alt="Midone - HTML Admin Template" class="rounded-full" src="https://enigma.left4code.com/dist/images/profile-8.jpg">
-                    <div class="absolute mb-1 mr-1 flex items-center justify-center bottom-0 right-0 bg-primary rounded-full p-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" icon-name="camera" class="lucide lucide-camera w-4 h-4 text-white" data-lucide="camera"><path d="M14.5 4h-5L7 7H4a2 2 0 00-2 2v9a2 2 0 002 2h16a2 2 0 002-2V9a2 2 0 00-2-2h-3l-2.5-3z"></path><circle cx="12" cy="13" r="3"></circle></svg>
-                    </div>
+                <div class="w-32 h-32 image-fit lg:mr-1">
+                    <i class="border-2 rounded-full w-full h-full py-1" data-lucide="user"></i>
                 </div>
                 <div class="ml-5">
                     <div class="w-24 sm:w-40 truncate sm:whitespace-normal font-medium text-lg">{{ $client->name }}</div>
